73479051e0e9b4a299dd71fee271e10ccc26de74
strongfuturetech <strongfuturelv@outlook.com>
2026-06-17 22:40:22 -0700

    Working on #2 - balls are gathered, and sorta follow.

--- main.ts
+++ main.ts
@@ -1,3 +1,56 @@
-scene.setBackgroundImage(assets.image`gameBG`)
-let yoshi = sprites.create(assets.image`yoshi`, SpriteKind.Player)
-controller.moveSprite(yoshi, 100, 0)
+namespace SpriteKind {
+    export const Pickup = SpriteKind.create()
+    export const Debug = SpriteKind.create()
+    export const Tool = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Pickup, function (sprite, otherSprite) {
+    otherSprite.setFlag(SpriteFlag.Ghost, true)
+    if (following.length == 0) {
+        console.logValue("Empty Following", following.length)
+        otherSprite.follow(followerSprite, 40)
+    } else {
+        console.logValue("We Have Followers", following.length)
+        otherSprite.follow(following[following.length], 40)
+    }
+    following.push(otherSprite)
+})
+function calcDistFromPlayer (sprite: Sprite) {
+    distanceFromPlayer = Math.sqrt((sprite.x - yoshi.x) ** 2 + (sprite.y - yoshi.y) ** 2)
+    return distanceFromPlayer
+}
+function calcSpriteDist (follower: Sprite, leader: Sprite) {
+    distFromLeader = Math.sqrt((follower.x - leader.x) ** 2 + (follower.y - leader.y) ** 2)
+    return distFromLeader
+}
+function spawnBasketballs () {
+    for (let ballSpot of tiles.getTilesByType(sprites.jewels.jewel2)) {
+        ball = sprites.create(assets.image`basketball`, SpriteKind.Pickup)
+        tiles.placeOnTile(ball, ballSpot)
+        tiles.setTileAt(ballSpot, assets.tile`baseTransparency16`)
+    }
+}
+let ball: Sprite = null
+let distFromLeader = 0
+let distanceFromPlayer = 0
+let followerSprite: Sprite = null
+let following: Sprite[] = []
+let yoshi: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+yoshi = sprites.create(assets.image`yoshi`, SpriteKind.Player)
+yoshi.ay = 300
+controller.moveSprite(yoshi, 70, 50)
+scene.cameraFollowSprite(yoshi)
+spawnBasketballs()
+following = []
+followerSprite = sprites.create(assets.image`followerHead`, SpriteKind.Tool)
+followerSprite.z = 25
+followerSprite.ay = 150
+let followDistance = 20
+// Chain Follow
+game.onUpdate(function () {
+    if (calcSpriteDist(followerSprite, yoshi) > followDistance) {
+        followerSprite.follow(yoshi)
+    } else {
+        followerSprite.unfollow()
+    }
+})
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,10 +2,36 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`14000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000101010101000000000000000000000000000001000000000000000000000000000000000001010100000000000000000000000000000101010100000000000000000000000000000001000000000000000001000200000000000000000000000000000000000001010000000000010000000000000000000000000101000000000001010100000000000000000002010101010100000101010101010101010101010101010101010101`, img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . 2 2 2 2 2 . . . . . 
+. . . . . . . . . 2 . . . . . . . . . . 
+. . . . . . . 2 2 2 . . . . . . . . . . 
+. . . . 2 2 2 2 . . . . . . . . . . . . 
+. . . 2 . . . . . . . . 2 . . . . . . . 
+. . . . . . . . . . . . . 2 2 . . . . . 
+2 . . . . . . . . . . . . 2 2 . . . . . 
+. 2 2 . . . . . . . . . . 2 2 2 2 2 . . 
+. . 2 2 2 2 2 2 2 2 2 2 2 2 . . . . 2 2 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.jewels.jewel2], TileScale.Sixteen);
+        }
+        return null;
+    })
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "baseTransparency16":
             case "transparency16":return transparency16;
+            case "myTile5":
+            case "tile1":return tile1;
         }
         return null;
     })
