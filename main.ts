c731f66d10fcbdba96a5272fc54c964f609f4a5b
strongfuturetech <strongfuturelv@outlook.com>
2026-06-22 17:47:03 -0700

    #1 - can technically fire the ball, just needs some polishing

--- main.ts
+++ main.ts
@@ -6,10 +6,24 @@ namespace SpriteKind {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Pickup, function (sprite, otherSprite) {
     otherSprite.setKind(SpriteKind.Follower)
+    // Turn this on if losing the connection is more common.
     otherSprite.setFlag(SpriteFlag.Ghost, false)
     otherSprite.ay = 100
     following.push(otherSprite)
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (following.length > 0) {
+        pick = following.pop()
+        pick.unfollow()
+        ballToShoot = darts.create(pick.image, SpriteKind.Pickup)
+        // we're gonna need to know which way Yoshi is facing so we can put it on the right side.
+        ballToShoot.setPosition(yoshi.x + 25, yoshi.y)
+        // Maybe we'll introduce a more interesting despawn anim later
+        pick.scale += -0.75
+        sprites.destroy(pick, effects.confetti, 500)
+    }
+})
+// I do want to borrow that "attempt jump" code
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     yoshi.vy = -115
 })
@@ -21,6 +35,10 @@ function calcSpriteDist (follower: Sprite, leader: Sprite) {
     distFromLeader = Math.sqrt((follower.x - leader.x) ** 2 + (follower.y - leader.y) ** 2)
     return distFromLeader
 }
+controller.B.onEvent(ControllerButtonEvent.Repeated, function () {
+    ballToShoot.pow += 10
+    pause(100)
+})
 function spawnBasketballs () {
     for (let ballSpot of tiles.getTilesByType(sprites.jewels.jewel2)) {
         ball = sprites.create(assets.image`basketball`, SpriteKind.Pickup)
@@ -28,13 +46,19 @@ function spawnBasketballs () {
         tiles.setTileAt(ballSpot, assets.tile`baseTransparency16`)
     }
 }
+controller.B.onEvent(ControllerButtonEvent.Released, function () {
+    ballToShoot.throwDart()
+})
 let leaderToCheck: Sprite = null
 let followerToCheck: Sprite = null
 let ball: Sprite = null
 let distFromLeader = 0
 let distanceFromPlayer = 0
+let ballToShoot: Dart = null
+let pick: Sprite = null
 let following: Sprite[] = []
 let yoshi: Sprite = null
+scene.setBackgroundImage(assets.image`gameBG`)
 tiles.setCurrentTilemap(tilemap`level1`)
 yoshi = sprites.create(assets.image`yoshi`, SpriteKind.Player)
 yoshi.ay = 300
@@ -54,7 +78,7 @@ game.onUpdate(function () {
                 leaderToCheck = following[index - 1]
             }
             if (calcSpriteDist(followerToCheck, leaderToCheck) > followDistance) {
-                followerToCheck.follow(leaderToCheck, 75)
+                followerToCheck.follow(leaderToCheck, 100)
             } else {
                 followerToCheck.unfollow()
             }
